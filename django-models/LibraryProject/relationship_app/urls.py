355a09f8ae10e0040fa09d69df2ae95ecebbaa7a
UDUKUNDA <udukundaelysee@gmail.com>
2025-11-23 03:38:35 +0200

    fix urls

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 from .views import (
     list_books,
-    LibraryDetailView,
-    register_view,
-    login_view,
-    logout_view,
+    LibraryDetailView,  
 )
+from . import views
+from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     # Function-based view URL
     path('books/', list_books, name='list_books'),
@@ -13,8 +12,8 @@ urlpatterns = [
     # Class-based view URL (library detail)
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 
-    # Authentication views
-    path('register/', register_view, name='register'),
-    path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout'),
+     # Authentication views
+    path('register/', views.register_view, name='register'),  # ALX expects 'views.register' here
+    path('login/', LoginView.as_view(template_name="relationship_app/login.html"), name='login'),
+    path('logout/', LogoutView.as_view(template_name="relationship_app/logout.html"), name='logout'),
 ]
