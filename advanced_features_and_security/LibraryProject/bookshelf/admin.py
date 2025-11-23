9743c0f125a1e300f01670007ce8ac83aa5c04d6
UDUKUNDA <udukundaelysee@gmail.com>
2025-11-23 18:46:01 +0200

    fix admin

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,8 +1,32 @@
 from django.contrib import admin
 from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, UserProfile, Author, Book, Library, Librarian
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ("title", "author", "publication_year")
     search_fields = ("title", "author")
     list_filter = ("publication_year",)
+# -------------------------------
+# CustomUser Admin
+# -------------------------------
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    # Add your custom fields to the admin forms
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+# Register CustomUser
+admin.site.register(CustomUser, CustomUserAdmin)
+
+# Optional: Register UserProfile and other models
+admin.site.register(UserProfile)
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(Library)
+admin.site.register(Librarian)
