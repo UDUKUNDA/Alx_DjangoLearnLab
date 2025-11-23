2ab7de69f4e9d2312d3b1484bd9ea81092829cbf
UDUKUNDA <udukundaelysee@gmail.com>
2025-11-23 20:14:24 +0200

    fix securities

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -171,6 +171,7 @@ X_FRAME_OPTIONS = "DENY"                     # Prevent clickjacking
 SECURE_CONTENT_TYPE_NOSNIFF = True          # Prevent MIME type sniffing
 SECURE_BROWSER_XSS_FILTER = True            # Enable basic XSS protection
 
-
+# Trust HTTPS header from proxy (e.g., Nginx)
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
