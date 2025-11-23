337593919d8497d7910a671d23db21d656499f45
UDUKUNDA <udukundaelysee@gmail.com>
2025-11-23 19:09:07 +0200

    fix midel

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,12 +1,10 @@
-from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Book
 
-# Create your views here.
 # View to list books
 @permission_required('bookshelf.can_view', raise_exception=True)
-def list_books(request):
+def book_list(request):
     books = Book.objects.all()
     return render(request, 'bookshelf/list_books.html', {'books': books})
 
@@ -17,7 +15,7 @@ def add_book(request):
         title = request.POST.get('title')
         author_id = request.POST.get('author')
         Book.objects.create(title=title, author_id=author_id)
-        return redirect('list_books')
+        return redirect('book_list')
     return render(request, 'bookshelf/add_book.html')
 
 # View to edit a book
@@ -27,7 +25,7 @@ def edit_book(request, pk):
     if request.method == "POST":
         book.title = request.POST.get('title')
         book.save()
-        return redirect('list_books')
+        return redirect('book_list')
     return render(request, 'bookshelf/edit_book.html', {'book': book})
 
 # View to delete a book
@@ -35,4 +33,4 @@ def edit_book(request, pk):
 def delete_book(request, pk):
     book = get_object_or_404(Book, pk=pk)
     book.delete()
-    return redirect('list_books')
+    return redirect('book_list')
