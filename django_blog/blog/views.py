d49a58da06454a421853aa7700d8f400d6b3c27d
UDUKUNDA <udukundaelysee@gmail.com>
2025-12-30 10:16:02 +0200

    Implementing the Blog User Authentication System

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .models import Post
+from .forms import CustomUserCreationForm, UserUpdateForm
 
 # Create your views here.
 def home(request):
@@ -11,8 +14,30 @@ def posts(request):
     # For now just return a simple response or we could create a template
     return HttpResponse("Here are the posts")
 
-def login_view(request):
-    return HttpResponse("Login Page")
-
 def register(request):
-    return HttpResponse("Register Page")
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('login')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        if u_form.is_valid():
+            u_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+    
+    context = {
+        'u_form': u_form
+    }
+    return render(request, 'blog/profile.html', context)
