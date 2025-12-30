9f15b170202df5dbadecb21b21ad7d20507cde3f
UDUKUNDA <udukundaelysee@gmail.com>
2025-12-30 10:20:47 +0200

    Creating Blog Post Management Features

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,12 +1,24 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+    register,
+    profile
+)
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('posts/', views.posts, name='posts'),
+    path('', PostListView.as_view(), name='posts'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
-    path('register/', views.register, name='register'),
-    path('profile/', views.profile, name='profile'),
+    path('register/', register, name='register'),
+    path('profile/', profile, name='profile'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 from .models import Post
 from .forms import CustomUserCreationForm, UserUpdateForm
 
@@ -9,10 +17,52 @@ from .forms import CustomUserCreationForm, UserUpdateForm
 def home(request):
     return render(request, 'blog/base.html')
 
-def posts(request):
-    posts = Post.objects.all()
-    # For now just return a simple response or we could create a template
-    return HttpResponse("Here are the posts")
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-published_date']
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+        
+    def get_success_url(self):
+         return '/'
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+    
+    def get_success_url(self):
+         return '/'
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 def register(request):
     if request.method == 'POST':
