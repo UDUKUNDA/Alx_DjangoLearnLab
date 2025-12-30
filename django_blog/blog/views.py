e2b0e0f4a17c38fa7ca9ccaabf8f3adf7030b67e
UDUKUNDA <udukundaelysee@gmail.com>
2025-12-30 10:06:25 +0200

    fix: databse config

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
+def home(request):
+    return render(request, 'blog/base.html')
+
+def posts(request):
+    posts = Post.objects.all()
+    # For now just return a simple response or we could create a template
+    return HttpResponse("Here are the posts")
+
+def login_view(request):
+    return HttpResponse("Login Page")
+
+def register(request):
+    return HttpResponse("Register Page")
